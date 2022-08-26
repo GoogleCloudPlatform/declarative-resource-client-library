d0bfa6bd154ec0af6cc3147c46d32ee450cc5515
DCL Team <no-reply@google.com>
2022-08-26 15:46:16 -0700

    Automated DCL import.
      - 97b833451f8333f884ed4126c003e1be6dde045a Automatic import from cloud_mmv2_dcl_20220826_1531_RC00 by DCL Team <no-reply@google.com>
    
    GitOrigin-RevId: 97b833451f8333f884ed4126c003e1be6dde045a

--- python/services/eventarc/alpha/register.go
+++ python/services/eventarc/alpha/register.go
@@ -21,4 +21,5 @@ import (
 // RegisterServers registers each resource with the gRPC server.
 func RegisterServers(s *grpc.Server) {
 	sdkgrpc.RegisterEventarcAlphaTriggerServiceServer(s, &TriggerServer{})
+	sdkgrpc.RegisterEventarcAlphaChannelServiceServer(s, &ChannelServer{})
 }
--- python/services/eventarc/beta/register.go
+++ python/services/eventarc/beta/register.go
@@ -21,4 +21,5 @@ import (
 // RegisterServers registers each resource with the gRPC server.
 func RegisterServers(s *grpc.Server) {
 	sdkgrpc.RegisterEventarcBetaTriggerServiceServer(s, &TriggerServer{})
+	sdkgrpc.RegisterEventarcBetaChannelServiceServer(s, &ChannelServer{})
 }
