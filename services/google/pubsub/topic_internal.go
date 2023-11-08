3a468daa8f0256b721b1b83f22b81e7bb0de3842
Lars Lehtonen <lars.lehtonen@gmail.com>
2023-11-08 11:15:46 -0800

    services/google/pubsub: fix dropped errors

--- services/google/pubsub/topic_internal.go
+++ services/google/pubsub/topic_internal.go
@@ -1,11 +1,11 @@
 // Copyright 2023 Google LLC. All Rights Reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -341,6 +341,9 @@ func (op *createTopicOperation) do(ctx context.Context, r *Topic, c *Client) err
 		}
 		return getResp, nil
 	}, c.Config.RetryProvider)
+	if err != nil {
+		return err
+	}
 
 	if _, err := c.GetTopic(ctx, r); err != nil {
 		c.Config.Logger.WarningWithContextf(ctx, "get returned error: %v", err)
