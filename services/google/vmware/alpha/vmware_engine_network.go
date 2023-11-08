32c04e88a4fba05c2e56af980de06e7ec4d49b11
Lars Lehtonen <lars.lehtonen@gmail.com>
2023-11-08 11:39:22 -0800

    gofmt entire project

--- services/google/vmware/alpha/vmware_engine_network.go
+++ services/google/vmware/alpha/vmware_engine_network.go
@@ -1,11 +1,11 @@
 // Copyright 2023 Google LLC. All Rights Reserved.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,8 +20,8 @@ import (
 	"fmt"
 	"time"
 
-	"google.golang.org/api/googleapi"
 	"github.com/GoogleCloudPlatform/declarative-resource-client-library/dcl"
+	"google.golang.org/api/googleapi"
 )
 
 type VmwareEngineNetwork struct {
